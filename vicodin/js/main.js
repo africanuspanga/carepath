6534bb8201baac22c07c4cc6398166adc944ba8a
Africanus Panga <africanuspanga@gmail.com>
2026-07-07 16:51:44 +0300

    CarePath overhaul: enquiry cart, product-style services/products, contact map, clients stats, rebrand cleanup, mobile fixes

--- vicodin/js/main.js
+++ vicodin/js/main.js
@@ -2049,7 +2049,7 @@
     -------------------------------------------------------- */
     $(window).on('scroll',function() {    
         var scroll = $(window).scrollTop();
-        if (scroll < 445) {
+        if (scroll < 80) {
             $(".ltn__header-sticky").removeClass("sticky-active");
         } else {
             $(".ltn__header-sticky").addClass("sticky-active");
